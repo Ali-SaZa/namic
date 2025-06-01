942f14380e9a1827ae430d8246eedb9b970e3943
alisaza <alizabihi1997@gmail.com>
2025-06-01 18:08:59 +0330

    add file

---
+++ src/utils/CoinToGeram.js
@@ -0,0 +1,18 @@
+export const weights = [
+  {
+    name: 'سکه تمام',
+    weight: 9.7596
+  },
+  {
+    name: 'نیم سکه',
+    weight: 4.8798
+  },
+  {
+    name: 'ربع سکه',
+    weight: 2.4399
+  },
+  {
+    name: 'سکه یک گرمی',
+    weight: 1.21995
+  }
+]
